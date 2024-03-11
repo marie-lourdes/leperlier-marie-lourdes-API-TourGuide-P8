1db69c55603ca3e668ed6500c23e2bfb6fa1f622
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-03-11 23:08:22 +0100

    UserService.java: add method CRUD  of data User from TourGuideService

--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/UserService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/UserService.java
@@ -1,8 +1,29 @@
 package com.openclassrooms.tourguide.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+
+import com.openclassrooms.tourguide.user.User;
 
 @Service
 public class UserService {
+	private final Map<String, User> internalUserMap = new HashMap<>();
+	
+	public User getUser(String userName) {
+		return internalUserMap.get(userName);
+	}
 
+	public List<User> getAllUsers() {
+		return internalUserMap.values().stream().collect(Collectors.toList());
+	}
+	
+	public void addUser(User user) {
+		if (!internalUserMap.containsKey(user.getUserName())) {
+			internalUserMap.put(user.getUserName(), user);
+		}
+	}
 }
