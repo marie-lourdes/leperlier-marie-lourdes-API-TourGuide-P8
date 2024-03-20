4cfe7f569906cb263843a2ff9713f79d2ee75f22
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-03-20 15:48:53 +0100

    GpsUtil.java: add methods getAllAttractions() and getOneAttraction(String attractionName)

--- src/main/java/com/openclassrooms/tourguide/service/GpsUtilService.java
+++ src/main/java/com/openclassrooms/tourguide/service/GpsUtilService.java
@@ -1,21 +1,33 @@
 package com.openclassrooms.tourguide.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.openclassrooms.tourguide.model.User;
 
 import gpsUtil.GpsUtil;
+import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
 
 @Service
 public class GpsUtilService {
 	private final GpsUtil gpsUtil;
+
 	public GpsUtilService(GpsUtil gpsUtil) {
 		this.gpsUtil = gpsUtil;
 	}
-	
-	public VisitedLocation  getUserVisitedLocation(User user) {
+
+	public VisitedLocation getUserVisitedLocation(User user) {
 		return gpsUtil.getUserLocation(user.getUserId());
 	}
-	
+
+	public List<Attraction> getAllAttractions() {
+		return gpsUtil.getAttractions();
+	}
+
+	public Attraction getOneAttraction(String attractionName) {
+		return gpsUtil.getAttractions().stream().filter(element -> element.attractionName.equals(attractionName))
+				.findFirst().orElseThrow(() -> new NullPointerException("Attraction not found"));
+	}
 }
