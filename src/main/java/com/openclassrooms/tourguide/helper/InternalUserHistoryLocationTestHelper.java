70db1fc8fb67c17accd1f8abb2f5148a4f7508c6
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-03-25 18:07:53 +0100

    Tracker.java: clean code

--- src/main/java/com/openclassrooms/tourguide/helper/InternalUserHistoryLocationTestHelper.java
+++ src/main/java/com/openclassrooms/tourguide/helper/InternalUserHistoryLocationTestHelper.java
@@ -2,9 +2,7 @@ package com.openclassrooms.tourguide.helper;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -14,20 +12,21 @@ import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
 
 public class InternalUserHistoryLocationTestHelper {
-	private static double randomLatitude=generateRandomLatitude();
-	private static double randomLongitude =generateRandomLongitude();
-	private static Date  randomTime=getRandomTime();
-	private InternalUserHistoryLocationTestHelper() {}
-	
-	public static void setUserHistoryLocation(User user) {
-		IntStream.range(0, 3).forEach(i -> {
-			user.addToVisitedLocations(new VisitedLocation(user.getUserId(),
-					new Location(randomLatitude, randomLongitude ), randomTime));
-		});
-	
+	private static double randomLatitude = generateRandomLatitude();
+	private static double randomLongitude = generateRandomLongitude();
+	private static Date randomTime = getRandomTime();
+
+	private InternalUserHistoryLocationTestHelper() {
 	}
 
-	private static  double generateRandomLongitude() {
+	public static void setUserHistoryLocation(User user) {
+		IntStream.range(0, 3).forEach(i -> {
+			user.addToVisitedLocations(
+					new VisitedLocation(user.getUserId(), new Location(randomLatitude, randomLongitude), randomTime));
+		});
+	}
+
+	private static double generateRandomLongitude() {
 		double leftLimit = -180;
 		double rightLimit = 180;
 		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
--- src/main/java/com/openclassrooms/tourguide/tracker/Tracker.java
+++ src/main/java/com/openclassrooms/tourguide/tracker/Tracker.java
@@ -10,37 +10,29 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.time.StopWatch;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.openclassrooms.tourguide.model.User;
 import com.openclassrooms.tourguide.service.GpsUtilService;
 import com.openclassrooms.tourguide.service.UserService;
 
-/* Les test appel 4 fois la creation d user avec tours guide service , 4 threads
- * au 1er  l appel de la creation d user de userService service le tracker demarre 
- * puis mis en attente le 2eme et le 3ème appel le tracker rencontre des erreurs de threads  et de lecture des users apres la 2eme et 3 ème appel de creation d user
- * et stoppe et recommence au dernier appel du TourGuideService pour la creation user
- * Le tout avec un seul thread et pas de pool pour gerer la mise en attente de plusieurs thread
- */
 public class Tracker extends Thread {
-	private final Logger logger = LoggerFactory.getLogger(Tracker.class);
-	//private Logger logger;
-	private static final long trackingPollingInterval = TimeUnit.SECONDS.toSeconds(5);//5 min (converti en secondes)trop long  la mise en attente du thread
+	 private static final Logger logger = LogManager.getLogger(Tracker.class);
+	private static final long trackingPollingInterval = TimeUnit.SECONDS.toSeconds(5);
 	private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 	private  UserService userService;
 	private GpsUtilService gpsUtilService;
 	 private String threadName;
-	private boolean stop = false;// !!!initilisez a true 
-	//-------------------------------
+	private boolean stop = false;
 	private final Map<User, Boolean> completedTrackingUsersMap = new HashMap<>();
 	
 	public Tracker(UserService userService,String threadName) {
 		this.userService = userService;
 		this.threadName = threadName;
-	//	this.logger = LoggerFactory.getLogger(Tracker.class);
 		executorService.submit(this);
 	}
+	
 	/*public Tracker(TourGuideService tourGuideService) {
 		this.tourGuideService = tourGuideService;	
 		executorService.submit(this);
@@ -56,19 +48,16 @@ public class Tracker extends Thread {
 	 * Assures to shut down the Tracker thread
 	 */
 	public void stopTracking() {
-		stop = true; // !!!reaffectez a false pour stopper le thread et renommer la varible avec run=
-						// false car un while (!stop=false) ne demarre pas la boucle et les instructions
-						// mais une valeur true
+		stop = true; 
 		executorService.shutdownNow();
 	}
-
 
 	@Override
 	public void run() {
 		StopWatch stopWatch = new StopWatch();
 		while (true) {
 			if (Thread.currentThread().isInterrupted() || stop) {
-				logger.debug("Tracker stopping {}" +threadName);
+				logger.debug("Tracker stopping {}",threadName);
 				break;		
 			}
 			
@@ -80,23 +69,20 @@ public class Tracker extends Thread {
 			}
 			
 			users.forEach(user -> completedTrackingUsersMap.put(user, false));
-			logger.debug("Begin Tracker. Tracking {} {}  users. " + threadName + users.size() );
+			logger.debug("Begin Tracker. Tracking {}, with {}  users. ",threadName,users.size() );
 			stopWatch.start();
 			users.forEach(user ->{
 				try {
 					gpsUtilService.trackUserLocation(user,userService);
 				} catch (ConcurrentModificationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					logger.error(e.getMessage());
 				} catch (InterruptedException e) {
 					logger.error("Tracker interrupted {}", threadName);
-					e.printStackTrace();
 				} catch (ExecutionException e) {
 					logger.error(e.getMessage());
 				}
 			});
 			
-			//-------------------------
 			boolean notFinished = true;
 			while(notFinished) {
 				try {
@@ -113,7 +99,7 @@ public class Tracker extends Thread {
 			}
 			
 			completedTrackingUsersMap.clear();
-//------------------------------			
+	
 			stopWatch.stop();
 			logger.debug("Tracker {} Time Elapsed: {}  seconds.", threadName,TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 			stopWatch.reset();
@@ -124,55 +110,10 @@ public class Tracker extends Thread {
 			} catch (InterruptedException e) {
 				break;
 			}
-		}
-		
+		}		
 	}
+	
 	public synchronized void finalizeTrackUser(User user) {
 		completedTrackingUsersMap.put(user, true);
 	}
-/*	@Override
-	public void run() {
-		StopWatch stopWatch = new StopWatch();
-		while (stop) {// ?? provoque boucle infinie si toujours a true*
-			// passez en parametre de la boucle while la variable stop , et a la fin de la
-			// method run pour stopper le thread reafectez la variable a false pour entrer a
-			// nouveau dans la boucle
-			try {
-				if (Thread.currentThread().isInterrupted()) {
-					// testez la condition sans la varinale stop et le placez dans lewhile pour
-					// testerl exception ConccurrentModificationexception
-					// le programme est interrompu si le thread est interrompu mais ne les thread
-					// attribut "interrupted" =false dans le debug donc ne devrait pas stopper le
-					// programme
-					logger.debug("Tracker stopping");
-					//stopTracking();
-					 break;
-				}
-
-				List<User> users = userService.getAllUsers();
-				logger.debug("Begin Tracker. Tracking " + users.size() + " users.");
-				stopWatch.start();
-				users.forEach(u -> {
-					try {
-						userService.trackUserLocation(u);
-					} catch (InterruptedException e) {
-						stopTracking();
-					}
-				});
-				stopWatch.stop();
-					
-				logger.debug(
-						"Tracker Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
-				stopWatch.reset();
-
-				logger.debug("Tracker sleeping");
-				TimeUnit.SECONDS.sleep(trackingPollingInterval);// ?? provoque des erreurs du 2 eme du 2eme appel de
-																// userService
-			} catch (InterruptedException e) {
-				logger.error(e.getMessage());
-				// stopTracking();
-			}
-			break;
-		}
-	}*/
 }
