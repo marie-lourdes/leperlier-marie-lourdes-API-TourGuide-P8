26e044dcd31f4cef8dc47eecb13fd2975ddc0a08
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-03-26 00:52:11 +0100

    ICalculatorRewardPoint.java: add method isNearAttraction(VisitedLocation visitedLocation, Attraction attraction),getDistance(Location loc1, Location loc2)

--- src/main/java/com/openclassrooms/tourguide/service/ICalculatorRewardPoint.java
+++ src/main/java/com/openclassrooms/tourguide/service/ICalculatorRewardPoint.java
@@ -1,8 +1,10 @@
 package com.openclassrooms.tourguide.service;
 
 import gpsUtil.location.Attraction;
+import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
 
 public interface ICalculatorRewardPoint {
 	 boolean isNearAttraction(VisitedLocation visitedLocation, Attraction attraction);
+	 public double getDistance(Location loc1, Location loc2);
 }
