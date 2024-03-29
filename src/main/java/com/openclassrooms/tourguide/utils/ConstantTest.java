2d17d602421d2bed2b1d31149a86e83ce7565005
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-03-29 15:01:31 +0100

    create utility class ConstantTest

--- src/main/java/com/openclassrooms/tourguide/utils/ConstantTest.java
+++ src/main/java/com/openclassrooms/tourguide/utils/ConstantTest.java
@@ -1,7 +1,7 @@
 package com.openclassrooms.tourguide.utils;
 
 public class ConstantTest {
-	private static final String TRIP_PRICER_API_KEY = "test-server-api-key";
+	public static final String TRIP_PRICER_API_KEY = "test-server-api-key";
 	
 	private ConstantTest (){
 		
