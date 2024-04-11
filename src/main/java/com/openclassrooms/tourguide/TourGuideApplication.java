53797fc35020b721bb9f3d1d684b1c69e9cb4d6b
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-11 16:43:08 +0200

    create class configuration for Swagger2  SwaggerConfig

--- src/main/java/com/openclassrooms/tourguide/TourGuideApplication.java
+++ src/main/java/com/openclassrooms/tourguide/TourGuideApplication.java
@@ -3,12 +3,9 @@ package com.openclassrooms.tourguide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
 @SpringBootApplication
 public class TourGuideApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(TourGuideApplication.class, args);
 	}
